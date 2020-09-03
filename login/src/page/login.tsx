3e5933a0691fa319aa2694f4e353b0952423772c
paedayz <pae16phasit@gmail.com>
2020-09-04 00:27:25 +0700

    [Created]: Login Form

--- login/src/page/login.tsx
+++ login/src/page/login.tsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 
-function login() {
-  return <div className="App">login</div>;
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.name == "username") {
+      setUsername(event.target.value);
+    } else {
+      setPassword(event.target.value);
+    }
+  };
+
+  return (
+    <div className="App">
+      <div>login</div>
+      <br />
+      <form>
+        <label>username : </label>
+        <input
+          onChange={handleOnChange}
+          name="username"
+          value={username}
+        ></input>
+        <br />
+        <label>password : </label>
+        <input
+          onChange={handleOnChange}
+          name="password"
+          value={password}
+        ></input>
+      </form>
+
+      <button>login</button>
+    </div>
+  );
 }
 
-export default login;
+export default Login;
